e9d4666310485eac7977897657a6eb333e60c615
David Haunschmied <david.haunschmied@hotmail.com>
2019-03-12 16:25:05 +0100

    Initial commit

---
+++ stockmanagement-api/src/main/java/pr/se/stockmanagementapi/StockmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package pr.se.stockmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanagementApiApplication.class, args);
+	}
+
+}
