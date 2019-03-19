1cfa91ca08f287d0f2f41d64b6331563558a858c
David Haunschmied <david.haunschmied@hotmail.com>
2019-03-19 13:33:11 +0100

    Angular material project setup

--- stockmanagement-app/src/app/app.module.ts
+++ stockmanagement-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material/material.module';
+import {CoreModule} from './core/core.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ stockmanagement-app/src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+@NgModule({
+  exports: [BrowserModule]
+})
+export class CoreModule {
+}
