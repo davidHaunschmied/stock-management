16e231fb1d968f7a00385193192a53142576f3f6
Thomas Hehenberger <hehenbergert@gmail.com>
2019-03-19 14:32:51 +0100

    Created component stock-list

--- stockmanagement-api/src/main/java/pr/se/stockmanagementapi/StockmanagementApiApplication.java
+++ stockmanagement-api/src/main/java/pr/se/stockmanagementapi/StockmanagementApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockmanagementApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockmanagementApiApplication.class, args);
+	    SpringApplication.run(StockmanagementApiApplication.class, args);
 	}
 
 }
--- stockmanagement-app/src/app/app.module.ts
+++ stockmanagement-app/src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {MaterialModule} from './material/material.module';
 import {CoreModule} from './core/core.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {StockListComponent} from './stock/stock-list/stock-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    CoreModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    MaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        StockListComponent
+    ],
+    imports: [
+        CoreModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
