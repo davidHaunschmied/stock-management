f2af2d20ec26cc676f6523518c49bc945eb2086b
David Haunschmied <david.haunschmied@hotmail.com>
2019-04-18 16:05:53 +0200

    * Fixed tests * Moved code for switching and creating stock depots to separate component * Save stock depot in local storage * Use Material icons from web instead of saving it locally

--- stockmanagement-api/src/main/java/pr/se/stockmanagementapi/model/StockExchange.java
+++ stockmanagement-api/src/main/java/pr/se/stockmanagementapi/model/StockExchange.java
@@ -1,5 +1,7 @@
 package pr.se.stockmanagementapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,7 @@ public class StockExchange {
     private String name;
 
     @OneToMany(mappedBy="stockExchange")
+    @JsonIgnore
     private List<Stock> stocks;
 
     public StockExchange() {
