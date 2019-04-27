ffdc4bc045ee2a4ded68e9f00f5d39d8a56c4296
David Haunschmied <david.haunschmied@hotmail.com>
2019-04-27 10:52:22 +0200

    Show figures in Euro instead of USD Removed unused fields

--- stockmanagement-api/src/main/java/pr/se/stockmanagementapi/dbsetup/TestDataGenerator.java
+++ stockmanagement-api/src/main/java/pr/se/stockmanagementapi/dbsetup/TestDataGenerator.java
@@ -4,26 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pr.se.stockmanagementapi.model.Depot;
 import pr.se.stockmanagementapi.respository.DepotRepository;
-import pr.se.stockmanagementapi.respository.StockExchangeRepository;
-import pr.se.stockmanagementapi.respository.StockRepository;
 
 @Component
 public class TestDataGenerator {
 
     private final DepotRepository depotRepository;
-    private final StockRepository stockRepository;
-    private final StockExchangeRepository stockExchangeRepository;
 
     @Autowired
-    public TestDataGenerator(DepotRepository depotRepository, StockRepository stockRepository, StockExchangeRepository stockExchangeRepository) {
+    public TestDataGenerator(DepotRepository depotRepository) {
         this.depotRepository = depotRepository;
-        this.stockRepository = stockRepository;
-        this.stockExchangeRepository = stockExchangeRepository;
     }
 
     public void fillDbWithTestData() {
         // Create test depots if empty
-        if (depotRepository.findAll().isEmpty()){
+        if (depotRepository.findAll().isEmpty()) {
             depotRepository.save(new Depot("Risikodepot"));
             depotRepository.save(new Depot("Sicherheitsdepot"));
         }
