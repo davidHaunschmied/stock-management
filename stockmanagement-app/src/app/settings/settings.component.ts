ead889cc4bc8b7ce073a18307d62e15257fc596a
Thomas Hehenberger <hehenbergert@gmail.com>
2019-05-31 15:43:46 +0200

    - settings UI

--- stockmanagement-app/src/app/settings/settings.component.ts
+++ stockmanagement-app/src/app/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ISettings} from "../model/ISettings";
 import {SettingsService} from "../services/settings/settings.service";
+import {MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'app-settings',
@@ -11,7 +12,8 @@ export class SettingsComponent implements OnInit {
 
   settings: ISettings;
 
-  constructor(private settingsService: SettingsService) { }
+  constructor(private settingsService: SettingsService,
+              private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.getSettings();
@@ -26,7 +28,15 @@ export class SettingsComponent implements OnInit {
   changeSettings(){
     this.settingsService.changeSettings(this.settings).subscribe(data => {
       this.settings = data;
+      this.openSnackBar(data);
       });
+  }
+
+  openSnackBar(data: ISettings) {
+    //this._snackBar.open("Kauf: " + data.relativePurchaseCharges + "% (mind. € " + data.flatPurchaseCharges + ") / Verkauf: " + data.relativeSellCharges + "% (mind. € " + data.flatSellCharges + ") ", "Close", {
+    this._snackBar.open("Erfolgreich gespeichert", "Schließen!", {
+    duration: 3000,
+    });
   }
 
 }
