8425e6298c7b0f743773ea5328bb41e55bd13fc1
Thomas Hehenberger <hehenbergert@gmail.com>
2019-05-31 23:47:07 +0200

    - alarm erstellbar

--- stockmanagement-app/src/app/stock/stock-details/stock-details.component.ts
+++ stockmanagement-app/src/app/stock/stock-details/stock-details.component.ts
@@ -4,6 +4,9 @@ import {IStock} from '../../model/IStock';
 import {ActivatedRoute, Router} from '@angular/router';
 import * as Highcharts from 'highcharts/highstock';
 import {IHistoryPoint} from "../../model/IHistoryPoint";
+import {AlarmCreateDialogComponent} from "../../alarm/alarm-create-dialog/alarm-create-dialog.component";
+import {AlarmService} from "../../services/alarm/alarm.service";
+import {MatDialog} from "@angular/material";
 
 @Component({
   selector: 'app-stock-details',
@@ -12,7 +15,6 @@ import {IHistoryPoint} from "../../model/IHistoryPoint";
 })
 export class StockDetailsComponent implements OnInit {
 
-  param: string;
   stock: IStock;
   stockHistory: IHistoryPoint [];
   Highcharts = Highcharts;
@@ -20,7 +22,9 @@ export class StockDetailsComponent implements OnInit {
 
   constructor(private stockService: StockService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private alarmService: AlarmService,
+              private createAlarmDialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -61,4 +65,24 @@ export class StockDetailsComponent implements OnInit {
       }
     );
   }
+
+  openCreateAlarmDialog(): void {
+    const dialogRef = this.createAlarmDialog.open(AlarmCreateDialogComponent, {
+      width: '300px',
+      data: {stock: this.stock, alarmPrice: this.stock.price},
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      if (data == null)
+        return;
+      this.alarmService.createAlarm(data.stock, data.alarmPrice).subscribe(
+        alarm => {
+          console.log('Added alarm of data: ' + JSON.stringify(alarm));
+        }, error => {
+          console.log('Error: ' + error.message);
+        }
+      );
+    });
+  }
+
 }
