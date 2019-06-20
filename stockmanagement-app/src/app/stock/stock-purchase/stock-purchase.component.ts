7715370af348ec70eb4da1a6bfc81fa3458e14a9
Thomas Hehenberger <hehenbergert@gmail.com>
2019-06-20 15:54:09 +0200

    - spesenberechnung anzeige logik

--- stockmanagement-app/src/app/stock/stock-purchase/stock-purchase.component.ts
+++ stockmanagement-app/src/app/stock/stock-purchase/stock-purchase.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {ISettings} from "../../model/ISettings";
+import {SettingsService} from "../../services/settings/settings.service";
 
 @Component({
   selector: 'app-stock-purchase',
@@ -8,15 +10,25 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 })
 export class StockPurchaseComponent implements OnInit {
   readonly initialTotalPrice: number;
+  settings: ISettings | any;
 
   constructor(
     public dialogRef: MatDialogRef<StockPurchaseComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public settingsService: SettingsService) {
     this.initialTotalPrice = this.data.totalPrice;
   }
 
   ngOnInit() {
+    this.getSettings();
+  }
 
+  getSettings(){
+    this.settingsService.getSettings().subscribe(
+      data => {
+        this.settings = data;
+      }
+    )
   }
 
   onNoClick(): void {
@@ -28,6 +40,10 @@ export class StockPurchaseComponent implements OnInit {
   }
 
   calculateTotalPrice() {
-    this.data.totalPrice = this.initialTotalPrice * this.data.amount;
+    if(this.initialTotalPrice*this.data.amount* ( 1 + this.settings.relativePurchaseCharges/100) > this.initialTotalPrice*this.data.amount + this.settings.flatPurchaseCharges) {
+      this.data.totalPrice = this.initialTotalPrice * this.data.amount * (1 + this.settings.relativePurchaseCharges / 100);
+    }else{
+      this.data.totalPrice = this.initialTotalPrice * this.data.amount + this.settings.flatPurchaseCharges;
+    }
   }
 }
